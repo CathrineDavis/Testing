180fd39bb30d2031771347b6fb42413b796a1f1d
CathrineDavis <67537381+CathrineDavis@users.noreply.github.com>
2020-06-28 10:44:21 +0200

    Delete $Sharecode javascript v.0.0.5.js

--- $Sharecode javascript v.0.0.5.js
+++
@@ -1,7 +0,0 @@
-javascript(windows.alert("Test");
-Selectionvar=window.getSelection().toString();
-sharecode=Selectionvar.split(/\r\n|\r|\n/g, 2)[1].slice(Selectionvar.split(/\r\n|\r|\n/g, 2)[1].indexOf("/"), Selectionvar.split(/\r\n|\r|\n/g, 2)[1].length)
-if (!("/" in sharecode)) {
-        sharecode=Selectionvar.split(/\r\n|\r|\n/g, 2)[2].slice(Selectionvar.split(/\r\n|\r|\n/g, 2)[2].indexOf("/"), Selectionvar.split(/\r\n|\r|\n/g, 2)[2].length)
-}
-function(){window.open("http"+Selectionvar.split("http", 2)[1].split(/\n/, 2)[0]+sharecode)})
