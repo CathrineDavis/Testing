f34912da04f45a159040e47defdd24a1b8404146
CathrineDavis <67537381+CathrineDavis@users.noreply.github.com>
2020-06-28 10:11:15 +0200

    Add files via upload

--- $Sharecode javascript v.0.0.5.js
+++ $Sharecode javascript v.0.0.5.js
@@ -1,6 +1,7 @@
-javascript:Selectionvar=window.getSelection().toString();
+javascript(windows.alert("Test");
+Selectionvar=window.getSelection().toString();
 sharecode=Selectionvar.split(/\r\n|\r|\n/g, 2)[1].slice(Selectionvar.split(/\r\n|\r|\n/g, 2)[1].indexOf("/"), Selectionvar.split(/\r\n|\r|\n/g, 2)[1].length)
 if (!("/" in sharecode)) {
         sharecode=Selectionvar.split(/\r\n|\r|\n/g, 2)[2].slice(Selectionvar.split(/\r\n|\r|\n/g, 2)[2].indexOf("/"), Selectionvar.split(/\r\n|\r|\n/g, 2)[2].length)
 }
-function(){window.open("http"+Selectionvar.split("http", 2)[1].split(/\n/, 2)[0]+sharecode)}
+function(){window.open("http"+Selectionvar.split("http", 2)[1].split(/\n/, 2)[0]+sharecode)})
